77b74e1050d8d065f9e9fa934401124ab2ab7bf0
Leonardo Silva <ldss1@outlook.com>
2023-10-22 12:18:22 -0300

    Home

---
+++ src/pages/Home/Index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { Container, Title } from '../../assets/styles/teste.js';
+
+function Home() {
+  return (
+	<Container>
+	  <Title>Home</Title>
+	  <Button title="Clique aquiJHJJ"/>
+	  <Button title="Clique aquiJHJJ"/>
+	</Container>
+  );
+}
+
+export default Home;
