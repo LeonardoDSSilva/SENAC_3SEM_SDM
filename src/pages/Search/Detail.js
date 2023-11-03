5c56679f28be9fd13fa11bf42ddb689fd58a522c
Leonardo Silva <ldss1@outlook.com>
2023-11-02 23:55:31 -0300

    Create Page Detail

--- src/pages/Search/Detail.js
+++ src/pages/Search/Detail.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ScrollView, Dimensions, Image } from 'react-native';
+
+const { width } = Dimensions.get('window');
+const height = width * 0.6; // 60%
 
 export default function Detail({ route, navigation }) {
+	const [imgActive, setImgActive] = useState(0);
+	onchange = (nativeEvent) => {
+		if (nativeEvent) {
+			const slide = Math.ceil(nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width);
+			if (slide !== imgActive) {
+				setImgActive(slide);
+			}
+		}
+	}
 	const item = route.params ? route.params.item : null;
 	return (
 		<View>
-			<Text>{item?.modelo}</Text>
+			<ScrollView
+				pagingEnabled
+				horizontal
+				onScroll={({ nativeEvent }) => onchange(nativeEvent)}
+				showsHorizontalScrollIndicator={false}
+				style={{ width, height}}>{
+					item.images.map((image, index) => (
+						<Image
+							key={index}
+							source={{ uri: image }}
+							style={{ width, height, resizeMode: 'cover' }}
+						/>
+					))
+				}</ScrollView>
+
+			<Text>{item.modelo}</Text>
+
+			
 		</View>
 	);
 }
