58e4743ca0b11c76499b7cf6bd12296580293380
Leonardo Silva <ldss1@outlook.com>
2023-11-16 23:57:23 -0300

    Update Car.js with random car model and add new sections for car and rental information
    - Set the car model to a random car from the carros data
    - Add new section for vehicle information displaying the license plate and autonomy
    - Add new section for battery percentage displaying remaining kilometers and battery percentage
    - Add button for recharging points
    - Add new section for rental information displaying days remaining, rental date, and return date
    - Add buttons for extending rental and returning the car
    
    This commit improves the display of car and rental information in the Car component.

--- src/pages/Car/Car.js
+++ src/pages/Car/Car.js
@@ -5,7 +5,7 @@ import carros from '../../data/carros.json';
 import { Ionicons } from '@expo/vector-icons';
 
 const myCar = {
-	model: carros[0],
+	model: carros[Math.floor(Math.random() * carros.length)],
 	placa: 'ABC-1234',
 	dataLocacao: '2023-09-01',
 	dataDevolucao: '2023-12-03',
@@ -34,8 +34,44 @@ export default function Car() {
 					<Ionicons name="battery-full" size={24} color="#000" />
 					<Text style={{color: '#000'}}>SOS</Text>
 				</TouchableOpacity>
-
 			</View>
+
+			<View style={styles.container1}>
+				<Text style={styles.secTitle}>Informações do veículo</Text>
+				<View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+					<Text style={styles.secText}>Placa: {myCar.placa}</Text>
+					<Text style={styles.secText}>Autonomia: {myCar.model.autonomia}</Text>
+				</View>
+			</View>
+
+			<View style={styles.container1}>
+				<Text style={styles.secTitle}>Porcentagem da bateria</Text>
+				<View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+					<Text style={styles.secText}>Km restantes: {myCar.model.autonomia.replace('km', '')* myCar.porcentagemBateria / 100}</Text>
+					<Text style={styles.secText}>Autonomia: {myCar.porcentagemBateria}%</Text>
+				</View>
+				<TouchableOpacity style={{backgroundColor: '#99CD85', padding: 12, paddingLeft: 24, paddingRight: 24, borderRadius: 20, marginRight: 20}} onPress={() => alert('Reservado')}>
+					<Text style={{color: '#000'}}>PONTOS DE RECARGA</Text>
+				</TouchableOpacity>
+			</View>
+
+			<View style={styles.container1}>
+				<View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+					<Text style={styles.secTitle}>Informações da locação</Text>
+					<Text style={styles.secText}>Dias restantes: {Math.floor((new Date(myCar.dataDevolucao) - new Date()) / (1000 * 60 * 60 * 24))}</Text>
+				</View>
+				<View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+					<Text style={styles.secText}>Locação: {myCar.dataLocacao}</Text>
+					<Text style={styles.secText}>Devolução: {myCar.dataDevolucao}</Text>
+				</View>
+				<TouchableOpacity style={{backgroundColor: '#99CD85', padding: 12, paddingLeft: 24, paddingRight: 24, borderRadius: 20, marginRight: 20}} onPress={() => alert('Reservado')}>
+					<Text style={{color: '#000'}}>EXTENDER</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={{backgroundColor: '#99CD85', padding: 12, paddingLeft: 24, paddingRight: 24, borderRadius: 20, marginRight: 20}} onPress={() => alert('Reservado')}>
+					<Text style={{color: '#000'}}>DEVOLVER</Text>
+				</TouchableOpacity>
+			</View>
+
 		</View>
 	);
 }
@@ -45,6 +81,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: '#fff',
 		padding: 20,
+		gap: 15
 	},
 	image: {
 		width: '100%',
@@ -66,10 +103,23 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		backgroundColor: '#99CD85',
 		height: 100,
-		padding: 12,
+		width: 105,
 		paddingLeft: 24,
 		paddingRight: 24,
 		borderRadius: 25,
-		marginLeft: 20,
+		marginLeft: 15,
+	},
+	container1: {
+		backgroundColor: '#F0EEE3',
+		flexDirection: 'column',
+		gap: 10,
+
+		paddingLeft: 5,
+		paddingRight: 5,
+		paddingTop: 15,
+		paddingBottom: 15,
+
+		borderRadius: 20,
+
 	},
 });
