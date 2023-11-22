5255a832da7f5ef59ba25a99051f19a5567226a3
Leonardo Silva <ldss1@outlook.com>
2023-11-21 23:53:40 -0300

    Refactor Search Detail page to use Carousel component
    This commit refactors the Search Detail page to replace the ScrollView and Image components with a Carousel component. This change improves the image scrolling experience and provides better navigation controls. Additionally, the commit adds a card header with the model name and brand, separates the car details and cost sections into their respective containers, and includes a "RESERVAR" button for making reservations. This enhances the overall layout and usability of the page. No relevant issues are associated with this commit.

--- src/pages/Search/Detail.js
+++ src/pages/Search/Detail.js
@@ -1,40 +1,57 @@
 import React, { useState } from 'react';
-import { View, Text, ScrollView, Dimensions, Image } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-const { width } = Dimensions.get('window');
-const height = width * 0.6; // 60%
+import Carousel from '../../components/Carousel';
 
 export default function Detail({ route, navigation }) {
-	const [imgActive, setImgActive] = useState(0);
-	onchange = (nativeEvent) => {
-		if (nativeEvent) {
-			const slide = Math.ceil(nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width);
-			if (slide !== imgActive) {
-				setImgActive(slide);
-			}
-		}
-	}
+
 	const item = route.params ? route.params.item : null;
 	return (
 		<View>
-			<ScrollView
-				pagingEnabled
-				horizontal
-				onScroll={({ nativeEvent }) => onchange(nativeEvent)}
-				showsHorizontalScrollIndicator={false}
-				style={{ width, height}}>{
-					item.images.map((image, index) => (
-						<Image
-							key={index}
-							source={{ uri: image }}
-							style={{ width, height, resizeMode: 'cover' }}
-						/>
-					))
-				}</ScrollView>
-
-			<Text>{item.modelo}</Text>
-
-			
+			<Carousel images={item.images}></Carousel>
+			<View style={styles.cardHeader}>
+				<Text style={styles.cardTitle}>{item.modelo}</Text>
+				<Text style={styles.cardSubtitle}>{item.marca}</Text>
+			</View>
+				<Text>Detalhes</Text>
+			<View style={styles.cardDetails}>
+				<Text>Autonomia: {item.autonomia}</Text>
+				<Text>Potência: {item.potencia}</Text>
+				<Text>Bateria: {item.bateria}</Text>
+				<Text>Tempo de recarga: {item.tempoRecarga}</Text>
+			</View>
+			<Text>Custo</Text>
+			<View style={styles.cardCusto}>
+				<Text>Por hora: {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(item.custo.hora)}</Text>
+				<Text>Por km rodado: {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(item.custo.kwRodado)}</Text>
+			</View>
+			<View style={{flexDirection: 'row', justifyContent: 'center', width: '100%', gap: 5}}>
+				<TouchableOpacity style={{backgroundColor: '#99CD85', padding: 12, paddingLeft: 24, paddingRight: 24, borderRadius: 20, marginRight: 20}} onPress={() => alert('Reservado')}>
+					<Text style={{color: '#000'}}>RESERVAR</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	cardHeader: {
+		padding: 10,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	cardTitle: {
+		fontSize: 24,
+		fontWeight: 'bold',
+	},
+	cardSubtitle: {
+		fontSize: 16,
+		color: '#a8a8a8',
+	},
+	cardDetails: {
+		padding: 10,
+	},
+	cardCusto: {
+		padding: 10,
+	},
+});
